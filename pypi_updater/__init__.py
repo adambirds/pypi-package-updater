7886cab5f2e92f556e3a59d96a5a1278b3d0738f
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2025-08-13 15:25:58 +0100

    config: Switch to scm_versioning.

--- pypi_updater/__init__.py
+++ pypi_updater/__init__.py
@@ -7,8 +7,6 @@ from .parser import Requirement, RequirementsParser
 from .pypi_client import PackageInfo, PyPIClient
 from .updater import PyPIUpdater, UpdateResult, UpdateSummary
 
-__version__ = "0.1.0"
-
 __all__ = [
     "PyPIUpdater",
     "RequirementsParser",
